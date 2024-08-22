a2fbdc63a7eb36de38c07255110343f742314885
Polash-Islam <polashprince2@gmail.com>
2024-08-22 19:46:15 +0600

    implement custom theme

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php 
+/*
+this template for displaying the header
+*/ 
+
+?>
+
+<!DOCTYPE html>
+<html lang="<?php language_attributes(  ); ?>" class="no-js">
+<head>
+    <meta charset="<?php  bloginfo( 'charset' )?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class() ?>>
+ <div id="header_are">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+            <a href=""><img src="<?php echo get_theme_mod('Polash_logo'); ?>" alt=""></a>
+            
+            </div>
+        </div>
+    </div>
+ </div>
+<?php wp_footer(); ?>
+</body>
+</html>
